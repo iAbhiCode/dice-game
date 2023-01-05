bb266f63e32035492fd2681c6312ce343a302711
Abhinav Pandey <code05@Coding.engine>
2023-01-05 13:12:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// alert("working fine");
+
+// var player1 = prompt("Player1 Name: ");
+// document.querySelector('p')[0].innerHTML = "player1";
+// var player2 = prompt("Player 2 Name: ");
+// document.querySelector('p')[1].innerHTML = "player2";
+
+
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 is Winner";
+}
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 is Winner";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!!!";
+}
